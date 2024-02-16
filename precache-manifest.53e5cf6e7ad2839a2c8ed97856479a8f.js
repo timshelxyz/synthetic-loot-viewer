74f08d6af89d17e5a641f2fa1438a2050050a3c6
Timshel <timshel@genesisproject.xyz>
2024-02-15 16:04:37 -0800

    Updates

---
+++ precache-manifest.53e5cf6e7ad2839a2c8ed97856479a8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "731f9e15b919cf84a3bcd8e016f178d0",
+    "url": "/synthetic-loot-viewer/index.html"
+  },
+  {
+    "revision": "9300fed5a56093638be2",
+    "url": "/synthetic-loot-viewer/static/css/main.81489f86.chunk.css"
+  },
+  {
+    "revision": "51296a1cd56dc9d6a13a",
+    "url": "/synthetic-loot-viewer/static/js/2.3530aef5.chunk.js"
+  },
+  {
+    "revision": "ef5844b258ca34801919a3a7c3b3b1ba",
+    "url": "/synthetic-loot-viewer/static/js/2.3530aef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9300fed5a56093638be2",
+    "url": "/synthetic-loot-viewer/static/js/main.055cc179.chunk.js"
+  },
+  {
+    "revision": "ff6df28d6364421fdd91",
+    "url": "/synthetic-loot-viewer/static/js/runtime-main.1f023c60.js"
+  }
+]);
